df7195e5d0bb77e18aaded2d8b3f256a3b2cf2e2
Christopher Comella <git.chrisco@gmail.com>
2015-09-22 22:45:59 +0200

    Tidy up link.rb, link_spec.rb, and spec_helper.rb

--- lib/link.rb
+++ lib/link.rb
@@ -2,17 +2,17 @@ class Link
 
   include DataMapper::Resource
 
-  property :id,         Serial
-  property :title,      String
-  property :url,        String
-  property :desc,       String
-  property :created_at, DateTime
+  property :id,                Serial
+  property :title,             String
+  property :url,               String
+  property :description,       String
+  property :created_at,        DateTime
 
   # belongs_to :user
   # has n, :tags, through: Resource
 
-  # validates_presence_of :title
-  # validates_presence_of :url
+  validates_presence_of :title
+  validates_presence_of :url
 
 end
 
--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -1,13 +1,11 @@
-# require './lib/link.rb'
+require 'link'
 
-# describe Link do
-#    it { is_expected.to have_property :id }
-#    it { is_expected.to have_property :title }
-#    it { is_expected.to have_property :url }
-#    it { is_expected.to have_property :description }
-
-#    it { is_expected.to have_many_and_belong_to :tags }
-
-#    it { is_expected.to validate_presence_of :title }
-#    it { is_expected.to validate_presence_of :url }
-# end
+describe Link do
+   it { is_expected.to have_property :id }
+   it { is_expected.to have_property :title }
+   it { is_expected.to have_property :url }
+   xit { is_expected.to have_property :description }
+   xit { is_expected.to have_many_and_belong_to :tags }
+   it { is_expected.to validate_presence_of :title }
+   it { is_expected.to validate_presence_of :url }
+end
